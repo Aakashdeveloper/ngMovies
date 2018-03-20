aabe84a5c198bb28e1d72e58a3a6ba99ee403d0f
Aakashdeveloper <aakash.h@hcl.com>
2018-03-21 02:07:01 +0530

    A

---
+++ apiAng.js
@@ -0,0 +1,42 @@
+/***************Fecth data using mongodb where its is realted to model***********/
+var express = require('express');
+var mongodb = require('mongodb').MongoClient;
+
+var app = express();
+
+var port = process.env.PORT||2200; 
+var commanRouter = express.Router();
+
+commanRouter.route('/getMovies')
+    .get(function(req,res){
+    	var url ="mongodb://bbhumi:bhumika@ds145380.mlab.com:45380/evan";
+    	mongodb.connect(url, (err, db) => {
+			  if (err) {
+			    return console.log(err);
+			  }
+			  db.collection('movies').find({}).toArray(
+            	function(err,data){
+					if(err)
+					   
+					   res.status(500).send(err);
+					else
+						res.setHeader('Access-Control-Allow-Origin','*')
+		    		    res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept')
+						res.json(data);
+		})
+		})
+		
+});
+commanRouter.route('/gettest')
+    .get(function(req,res){
+    	console.log("hiiiiiiiiiiiii")
+		
+});
+app.use('/api', commanRouter);
+
+app.get('/',function(req,res){
+	res.send("Working")
+});
+app.listen(port, function(){
+	console.log("running");
+});
